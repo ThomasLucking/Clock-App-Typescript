b16484a04c1ae238ba7ead885f2c8755736d5547
thomaslucking <telucking@gmail.com>
2026-03-31 11:11:59 +0200

    fix: changed timestamp to timestampz

--- Schema.sql
+++ Schema.sql
@@ -3,35 +3,36 @@ create table Projects(
     project_id int generated always as identity primary key,
     name text not null,
     description text not null,
-    created_at timestamp not null default current_timestamp,
-    updated_at timestamp not null default current_timestamp
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp
 
 );
 
 create table Labels(
     label_id int generated always as identity primary key,
     name varchar(255) not null,
-    created_at timestamp not null default current_timestamp,
-    updated_at timestamp not null default current_timestamp
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp
 );
 
 
 create table Time_entries(
     time_entry_id int generated always as identity primary key,
     project_id int not null,
-    start_time timestamp not null,
-    end_time timestamp not null,
-    created_at timestamp not null default current_timestamp,
-    updated_at timestamp not null default current_timestamp,
-    foreign key (project_id) references Projects(project_id) 
+    start_time timestamptz not null,
+    end_time timestamptz not null,
+    created_at timestamptz not null default current_timestamp,
+    updated_at timestamptz not null default current_timestamp,
+    foreign key (project_id) references Projects(project_id) on delete cascade,
+    constraint check_times check (end_time >= start_time)
 );
 
 
-create table time_entry_label(
+create table Time_entry_labels(
     primary key (time_entry_id, label_id),
     time_entry_id int not null,
     label_id int not null,
-    foreign key (time_entry_id) references Time_entries(time_entry_id),
-    foreign key (label_id) references Labels(label_id)
+    foreign key (time_entry_id) references Time_entries(time_entry_id) on delete cascade,
+    foreign key (label_id) references Labels(label_id) on delete cascade
 );
 
