628f813e449ec41b311ac0e7498e420cba482e8a
thomaslucking <telucking@gmail.com>
2026-04-28 09:02:48 +0200

    Revert "feat: finished labels endpoint"
    This reverts commit c4560d08e6ac775dabb22497ebfff80477c31c8c.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,11 @@
 import { Elysia } from "elysia";
-import { labelRoutes } from "./modules/labels/labels.route";
 import { projectRoutes } from "./modules/projects/project.route";
 
 const app = new Elysia()
   .use(projectRoutes)
-  .use(labelRoutes)
   .listen(3000)
 
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
-
-
-app.handle(new Request('http://localhost:3000/projects')).then(r => r.text().then(console.log))
-app.handle(new Request('http://localhost:3000/labels')).then(r => console.log(r.status)) // 404
